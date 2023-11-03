95857e7687886a9bb0bf9dd02562e36446b9adca
claramaq <142675284+claramaq@users.noreply.github.com>
2023-11-03 18:35:59 -0500

    fixed file routing

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ let map = L.map('map').setView(latlng,zoom);
             zoom = 17;
             map.panTo(latlng)
             map.setZoom(zoom)
-            image = `/images/watergardens.jpg`
+            image = `images/watergardens.jpg`
             title = `Fort Worth Water Gardens`;
             description = 'This is a great place to start adventuring in DFW. It\'s free (unless you pay for parking)!'
             marker = L.marker(latlng).addTo(map);
@@ -52,7 +52,7 @@ let map = L.map('map').setView(latlng,zoom);
             zoom = 15;
             map.panTo(latlng)
             map.setZoom(zoom)
-            image = `/images/sixflags.jpg`
+            image = `images/sixflags.jpg`
             title = `Six Flags Over Texas`;
             description = 'While amusement parks can be expensive, if you live in the area or are planning to go more than one day, grab a seasons pass! The total cost should be cheaper, and there\'s tons of fun to be had for the whole family.'
             marker = L.marker(latlng).addTo(map);
@@ -66,7 +66,7 @@ let map = L.map('map').setView(latlng,zoom);
             zoom = 15;
             map.panTo(latlng)
             map.setZoom(zoom)
-            image = `/images/stockyards.jpg`
+            image = `images/stockyards.jpg`
             title = `Fort Worth Stockyards`;
             description = 'This is a definite go if you\'ve never been to or lived on a farm. Here lives Cowboy hats, stock shows, and competitions as well as the occasional parade and plentiful of food and drink.'
             marker = L.marker(latlng).addTo(map);
@@ -80,7 +80,7 @@ let map = L.map('map').setView(latlng,zoom);
             zoom = 15;
             map.panTo(latlng)
             map.setZoom(zoom)
-            image = `/images/cowboys.jpg`
+            image = `images/cowboys.jpg`
             title = `AT&T Cowboys Stadium`;
             description = 'The Cowboys are one of the major teams in Texas, and this stadium stars most games. But there are more events than just games here. A lot of big name concerts are also held here. Just a word of warning: parking can get really expensive. Have a game plan ready, just in case.'
             marker = L.marker(latlng).addTo(map);
@@ -94,7 +94,7 @@ let map = L.map('map').setView(latlng,zoom);
             zoom = 15;
             map.panTo(latlng)
             map.setZoom(zoom)
-            image = `/images/fair.jpg`
+            image = `images/fair.jpg`
             title = `Dallas Fair Grounds`;
             description = 'These fair grounds are massive and there\'s always something going on. If you\'re in the area and free, check out to see if anything is going on here. There\'s also a train that runs back and forth from Fort Worth and Dallas that stops at the Fair Grounds for easier transportation.'
             marker = L.marker(latlng).addTo(map);
@@ -108,7 +108,7 @@ let map = L.map('map').setView(latlng,zoom);
             zoom = 15;
             map.panTo(latlng)
             map.setZoom(zoom)
-            image = `/images/7th.jpg`
+            image = `images/7th.jpg`
             title = `7th Street`;
             description = '7th Street is the Fort Worth Downtown Shopping Center. There are plenty of places to shop downtown, but 7th Street is definitely some of the best.'
             marker = L.marker(latlng).addTo(map);
@@ -122,7 +122,7 @@ let map = L.map('map').setView(latlng,zoom);
             zoom = 15;
             map.panTo(latlng)
             map.setZoom(zoom)
-            image = `/images/hMart.jpg`
+            image = `images/hMart.jpg`
             title = `New Korea Town - Carrollton`;
             description = 'Carrollton, Texas has some fantastic Asian businesses. There\'s amazing shopping and even better food! There\'s even a local HMart! With plans to build another in Haltom City. It\'s definitely a must go.'
             marker = L.marker(latlng).addTo(map);
